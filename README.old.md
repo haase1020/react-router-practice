1050a47ed360924dc70a700c995bc8f00ef8fa3d
Mandi <haase1020@gmail.com>
2021-02-23 14:02:33 -0500

    initial commit and entry point to having fun with routes

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-router-practice
+a simple react app for practicing react-router
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+  return <h1>hi there</h1>;
+}
+
+export default App;
